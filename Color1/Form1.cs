e18445dcf5996f6ba5bcaee04d7777a20c5e798d
Attesa <calstream.lgn@gmail.com>
2017-09-15 11:12:32 +0300

    d

--- Color1/Form1.cs
+++ Color1/Form1.cs
@@ -15,7 +15,8 @@ namespace Color1
 	{
 
 		string filepath = "";
-		 byte[] rgbValues;
+		byte[] rgbValues;
+
 
 		public Form1()
 		{
@@ -35,6 +36,43 @@ namespace Color1
 					pictureBox.Image.Dispose();
 				pictureBox.Image = new Bitmap(ofd.FileName);
 			}
+		}
+
+
+		private void simple_gs(Bitmap bmp)
+		{
+
+			Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+			System.Drawing.Imaging.BitmapData bmpData =
+				bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+				bmp.PixelFormat);
+
+			// Get the address of the first line.
+			IntPtr ptr = bmpData.Scan0;
+
+			// Declare an array to hold the bytes of the bitmap.
+			int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+			rgbValues = new byte[bytes];
+
+			// Copy the RGB values into the array.
+			System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+
+			for (int counter = 0; counter < rgbValues.Length; counter += 3)
+			{
+				byte b = rgbValues[counter];
+				byte g = rgbValues[counter + 1];
+				byte r = rgbValues[counter + 2];
+				byte gs = (byte)(0.33 * b + 0.33 * g + 0.33 * r);
+				rgbValues[counter] = gs;
+				rgbValues[counter + 1] = gs;
+				rgbValues[counter + 2] = gs;
+			}
+
+			System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+			// Unlock the bits.
+			bmp.UnlockBits(bmpData);
 		}
 
 		private void GSequal_Click(object sender, EventArgs e)
@@ -58,17 +96,17 @@ namespace Color1
 			// Copy the RGB values into the array.
 			System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
 
-			// Set every third value to 255. A 24bpp bitmap will look red.  
-			for (int counter = 0; counter < rgbValues.Length; counter += 3)
-			{
-				byte b = rgbValues[counter];
-				byte g = rgbValues[counter+1];
-				byte r = rgbValues[counter+2];
-				byte gs = (byte)(0.33 * b + 0.33 * g + 0.33 * r);
-				rgbValues[counter] = gs;
-				rgbValues[counter+1] = gs;
-				rgbValues[counter+2] = gs;
-			}
+			
+			//for (int counter = 0; counter < rgbValues.Length; counter += 3)
+			//{
+			//	byte b = rgbValues[counter];
+			//	byte g = rgbValues[counter+1];
+			//	byte r = rgbValues[counter+2];
+			//	byte gs = (byte)(0.33 * b + 0.33 * g + 0.33 * r);
+			//	rgbValues[counter] = gs;
+			//	rgbValues[counter+1] = gs;
+			//	rgbValues[counter+2] = gs;
+			//}
 			// Copy the RGB values back to the bitmap
 			System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
 
@@ -90,7 +128,6 @@ namespace Color1
 		{
 			Bitmap bmp = pictureBox.Image as Bitmap;
 
-
 			Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
 			System.Drawing.Imaging.BitmapData bmpData =
 				bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
@@ -106,17 +143,16 @@ namespace Color1
 			// Copy the RGB values into the array.
 			System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
 
-			// Set every third value to 255. A 24bpp bitmap will look red.  
-			for (int counter = 0; counter < rgbValues.Length; counter += 3)
-			{
-				byte b = rgbValues[counter];
-				byte g = rgbValues[counter + 1];
-				byte r = rgbValues[counter + 2];
-				byte gs = (byte)(0.0722 * b + 0.7152 * g + 0.2126 * r);
-				rgbValues[counter] = gs;
-				rgbValues[counter + 1] = gs;
-				rgbValues[counter + 2] = gs;
-			}
+			//for (int counter = 0; counter < rgbValues.Length; counter += 3)
+			//{
+			//	byte b = rgbValues[counter];
+			//	byte g = rgbValues[counter + 1];
+			//	byte r = rgbValues[counter + 2];
+			//	byte gs = (byte)(0.0722 * b + 0.7152 * g + 0.2126 * r);
+			//	rgbValues[counter] = gs;
+			//	rgbValues[counter + 1] = gs;
+			//	rgbValues[counter + 2] = gs;
+			//}
 			// Copy the RGB values back to the bitmap
 			System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
 
@@ -136,6 +172,12 @@ namespace Color1
 
 		private void GSdifference_Click(object sender, EventArgs e)
 		{
+			Bitmap gs = pictureBox.Image as Bitmap;
+			simple_gs(gs);
+			pictureBox.Image = gs;
+			pictureBox.Refresh();
+			Bitmap hdtv = pictureBox.Image as Bitmap;
+			Bitmap res = pictureBox.Image as Bitmap;
 
 		}
 
